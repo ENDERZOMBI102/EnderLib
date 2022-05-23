8dcc0b1d9a1ee0dd4e8d5e059f371ec4de8eeba9
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-05-23 11:29:06 +0200

    remove useless stuff

--- src/main/java/com/enderzombi102/enderlib/common/MathHelper.java
+++
@@ -1,9 +0,0 @@
-package com.enderzombi102.enderlib.common;
-
-public class MathHelper {
-
-	public static float interpolateValues(float prevVal, float nextVal, float partialTick) {
-		return prevVal + partialTick * (nextVal - prevVal);
-	}
-
-}

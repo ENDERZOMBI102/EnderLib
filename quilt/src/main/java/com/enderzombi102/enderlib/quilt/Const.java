d1dc9ddabcfae5178bd3c25af97ae5a7887a6899
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-06-06 18:03:16 +0200

    added quilt module

---
+++ quilt/src/main/java/com/enderzombi102/enderlib/quilt/Const.java
@@ -0,0 +1,16 @@
+package com.enderzombi102.enderlib.quilt;
+
+import net.minecraft.util.Identifier;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.QuiltLoader;
+
+public class Const {
+	public static final String ID = "enderlib-quilt";
+	@SuppressWarnings("OptionalGetWithoutIsPresent")
+	public static final ModContainer CONTAINER = QuiltLoader.getModContainer(ID).get();
+	public static final String VERSION = CONTAINER.metadata().version().raw();
+
+	public static Identifier getId( String key ) {
+		return new Identifier( ID, key );
+	}
+}
---
+++ quilt/src/main/java/com/enderzombi102/enderlib/quilt/compat/ModMenuCompat.java
@@ -0,0 +1,31 @@
+package com.enderzombi102.enderlib.quilt.compat;
+
+import com.terraformersmc.modmenu.ModMenu;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuCompat implements ModMenuApi {
+	static {
+		var loader = ModMenuCompat.class.getClassLoader();
+		if ( loader.getResource( "EnderLib/extensions/java/lang/String/String.class" ) != null )
+			// add manifold lib
+			ModMenu.MODS.put(
+				"enderlib-manifolds",
+				new EnderlibModuleMod(
+					"manifolds",
+					"Manifolds",
+					"EnderLib module containing various extension methods and stuff"
+				)
+			);
+		if ( loader.getResource( "com/enderzombi102/enderlib/Strings.class" ) != null )
+			// add main lib
+			ModMenu.MODS.put(
+				"enderlib-core",
+				new EnderlibModuleMod(
+					"core",
+					"Core",
+					"EnderLib main module, contains various utilities ENDER commonly use"
+				)
+			);
+	}
+}
+

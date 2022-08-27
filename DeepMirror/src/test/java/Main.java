4b70f1889ec306392326b9e54e28ac50908f4979
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-08-27 12:33:06 +0200

    testing stuff

--- DeepMirror/src/test/java/Main.java
+++ DeepMirror/src/test/java/Main.java
@@ -1,7 +1,12 @@
+import sun.jvm.hotspot.HotSpotAgent;
+
 public class Main {
 	public static void main( String[] argv ) {
 		System.out.println("Hello world!");
-		sun.jvm.hotspot.runtime.VM.shutdown();
-		System.out.println("Hello world 2!");
+		var x = new HotSpotAgent();
+		x.attach( 37000 );
+		var debugger= x.getDebugger();
+		var vm = x.getDebugger();
+		x.detach();
 	}
 }
